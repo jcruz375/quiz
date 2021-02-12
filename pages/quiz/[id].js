a6aaf94e8a8b7395ba52810baa2a9c15f7d8a8d2
jcruz375 <joaocruz375@gmail.com>
2021-02-12 11:17:19 -0300

    next api

---
+++ pages/quiz/[id].js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+
+const OtherQuizzesPage = () => {
+  return (
+    <>
+      <div>
+        teste teste
+      </div>
+    </>
+  );
+};
+
+export default OtherQuizzesPage;
